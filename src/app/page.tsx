04c0b83b1188fece0f36ea1bf48ad7a29432b6b8
edenawoke9 <135725390+edenawoke9@users.noreply.github.com>
2025-06-21 22:46:40 +0300

    intitialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,70 @@
-import Image from "next/image";
+'use client';
+import React, { useState, useRef } from "react";
+import { getYear, getISOWeeksInYear, setISOWeek, startOfISOWeek, endOfISOWeek, format } from 'date-fns';
+import Products from "../components/products/products";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col items-center justify-center bg-white">
+    <div className="bg-[#fafbfc] text-4xl min-h-screen w-1/2 flex flex-col  text-black font-sans">
+      <div>
+        <h1>Welcome To <span className="bg-gradient-to-r from-[#e14eca] to-[#3b82f6] bg-clip-text text-transparent">Tiny Startups</span></h1>
+        <p className="text-gray-500 text-xl">The launch platfrom for your tiny startups and side projects</p>
+      </div>
+      <div className="border-t mt-4 ">
+        <WeekList />
+      </div>
+      
+    </div>
+    </div>
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export function WeekList(){
+ 
+  const year=getYear(new Date());
+  const weeksInYear=getISOWeeksInYear(new Date())
+  const weeks=Array.from({length:weeksInYear},(_,i)=>{
+    let weekNum=i+1
+    const dateForWeek = setISOWeek(new Date(year, 0, 4), weekNum);
+    const startDate = startOfISOWeek(dateForWeek);
+    const endDate = endOfISOWeek(dateForWeek);
+    
+    return {
+      week: weekNum,
+      range: `${format(startDate, 'MMM d')} - ${format(endDate, 'MMM d')}`,
+    }
+  });
+  const [ActiveWeek,setWeek] = useState(weeks[22]);
+  const scrollContainer = useRef<HTMLDivElement>(null);
+
+  const scroll = (direction: 'left' | 'right') => {
+    if (scrollContainer.current) {
+      const scrollAmount = direction === 'left' ? -200 : 200;
+      scrollContainer.current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+  };
+
+  return(
+    <div className="w-full">
+      <div className="flex items-center gap-2 text-sm mt-4">
+       <button onClick={() => scroll('left')} className="p-2 text-xl text-gray-400 hover:text-black">
+        {'<'}
+      </button>
+      <div ref={scrollContainer} className="flex overflow-x-auto scroll-smooth items-center gap-x-4" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+      {weeks.map((week) => (
+        <div key={week.week} className={`flex-shrink-0 ${ActiveWeek.week === week.week ? 'bg-green-100 rounded-lg' : ''}`}>
+          <button onClick={()=>setWeek(week)} className={`whitespace-nowrap px-3 py-1.5 font-semibold ${ActiveWeek.week === week.week ? 'text-green-700' : 'text-gray-500'}`}>
+            Week {week.week}
+            </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
+      </div>
+       <button onClick={() => scroll('right')} className="p-2 text-xl text-gray-400 hover:text-black">
+        {'>'}
+      </button>
+      </div>
+      <Products ActiveWeek={ActiveWeek} />
     </div>
   );
 }
